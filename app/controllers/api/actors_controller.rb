2dbe1a50e527d170569530b9a35fe9c08474a518
jimmy <jimewilson87@gmail.com>
2020-09-13 23:02:28 -0500

    updates parameters

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,16 @@
 class Api::ActorsController < ApplicationController
   def single_actor
-    @single_actor = Actor.find_by(id: 6)
+    @actor = Actor.find(params[:id])
+    render "single_actor.json.jb"
+  end
+
+  def create_actor
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+    )
+    @actor.save
     render "single_actor.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/single_actor" => "actorss#single_actor"
+    get "/single_actor" => "actors#single_actor"
     get "/all_movies" => "movies#all_movies"
     get "single_movie" => "movies#single_movie"
+    get "/single_actor/:id" => "actors#single_actor"
+    post "/single_actor" => "actors#create_actor"
   end
 end
