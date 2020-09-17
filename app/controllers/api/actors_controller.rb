c5fec700ccb2fca81477d935da5fedee30eb8011
jimmy <jimewilson87@gmail.com>
2020-09-16 22:11:22 -0500

    updates attributes

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,16 +1,40 @@
 class Api::ActorsController < ApplicationController
-  def single_actor
-    @actor = Actor.find(params[:id])
-    render "single_actor.json.jb"
+  def index
+    @actors = Actor.all
+    render "index.json.jb"
   end
 
-  def create_actor
+  def show
+    @actor = Actor.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
     @actor = Actor.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age],
     )
     @actor.save
-    render "single_actor.json.jb"
+    render "show.json.jb"
+  end
+
+  def update
+    @actor = Actor.find_by(id: params[:id])
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
+    @actor.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    actor = Actor.find_by(id: params[:id])
+    actor.destroy
+    render json: { message: "DESTRUCTO! No more actor!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+
+    get "/actors" => "actors#index"
+    get "/actors/:id" => "actors#show"
+    post "/actors" => "actors#create"
+    patch "/actors/:id" => "actors#update"
+    delete "/actors/:id" => "actors#destroy"
   end
 end
