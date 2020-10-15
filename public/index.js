01b6f777a2b3af9de0988f2dbe8e7354b6b8d4eb
jimmy <jimewilson87@gmail.com>
2020-10-14 21:04:32 -0500

    makes heroku test

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 /* global axios */
 
-axios.get("http://localhost:3000/api/actors").then(function(response) {
+axios.get("https://dashboard.heroku.com/apps/glacial-garden-68567").then(function(response) {
   var actors = response.data;
   console.log(actors);
 });
--- public/moviesIndex.js
+++ public/moviesIndex.js
@@ -1,6 +1,6 @@
 /* global axios */
 
-axios.get("http://localhost:3000/api/movies").then(function(response) {
+axios.get("https://dashboard.heroku.com/apps/glacial-garden-68567").then(function(response) {
   var movies = response.data;
   console.log(movies);
 });
