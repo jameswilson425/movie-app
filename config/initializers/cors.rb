d5094693fac7727a76599d6612b6654ea3755579
jimmy <jimewilson87@gmail.com>
2020-10-15 23:49:22 -0500

    update check

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,13 +3,13 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-Read more: https://github.com/cyu/rack-cors
+# Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'example.com'
+    origins "localhost:8080", "example.com"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
